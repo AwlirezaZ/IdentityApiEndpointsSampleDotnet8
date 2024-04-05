f4b285a349cce3a4f36d50783b2924ac67f53e32
Alireza Ahmadi <ahmadialireza317@gmail.com>
2024-04-05 16:45:16 +0330

    init project with installing packages and dependencies

---
+++ IdentityApiEndpointsSampleDotnet8/Context/MyApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApiEndpointsSampleDotnet8.Context
+{
+    public class MyApplicationDbContext: IdentityDbContext
+    {
+        public MyApplicationDbContext(DbContextOptions<MyApplicationDbContext> options): base(options)
+        {
+            
+        }
+    }
+}
---
+++ IdentityApiEndpointsSampleDotnet8/Program.cs
@@ -0,0 +1,40 @@
+using IdentityApiEndpointsSampleDotnet8.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MyApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<MyApplicationDbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapIdentityApi<IdentityUser>();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
